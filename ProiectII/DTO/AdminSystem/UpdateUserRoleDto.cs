4b694b7d96a57cec6cc1bcf6179e8ae030606559
cimpean-victorS-30125 <victor.github.utcn@gmail.com>
2026-04-11 23:36:40 +0300

    commit

---
+++ ProiectII/DTO/AdminSystem/UpdateUserRoleDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProiectII.DTO.AdminSystem
+{
+    public class UpdateUserRoleDto
+    {
+        public uint UserId { get; set; }
+        // de vazut aici 
+        //TODO de vazut exact!!
+
+        public string NewRole { get; set; } = string.Empty;
+    }
+}
---
+++ ProiectII/DTO/AdminSystem/UserStatusUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ProiectII.DTO.AdminSystem
+{
+    public class UserStatusUpdateDto
+    {
+        public uint UserId { get; set; }
+        public bool IsActive { get; set; }
+        public string? Reason { get; set; }
+    }
+}
