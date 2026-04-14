25acb0a48c8a665bf43ead2125f0f64fb10da6ea
cimpean-victorS-30125 <victor.github.utcn@gmail.com>
2026-04-14 21:48:11 +0300

    commit AUTO BAT file initiliazer

--- ProiectII/manage.bat
+++ ProiectII/manage.bat
@@ -19,37 +19,36 @@ IF %M%==2 GOTO STOP
 IF %M%==3 GOTO RESET
 IF %M%==4 GOTO MIGRATE
 IF %M%==5 GOTO MYSQL
-IF %M%==6 GOTO EOF
+IF %M%==6 GOTO EXIT
 
 
 :START
-ECHO --> Pornire containere...
+ECHO  Pornire containere...
 docker-compose up -d
-ECHO --> Asteptare pornire MySQL (10 secunde)...
+ECHO  Asteptare pornire MySQL (10 secunde)...
 timeout /t 10 /nobreak
 GOTO MIGRATE
 
 :STOP
-ECHO --> Oprire containere...
+ECHO  Oprire containere...
 docker-compose stop
 GOTO MENU
 
 :RESET
-ECHO --> Stergere totala date si containere...
+ECHO  Stergere totala date si containere...
 docker-compose down -v
 GOTO MENU
 
 :MIGRATE
-ECHO --> Aplicare migrari baza de date...
+ECHO  Aplicare migrari baza de date...
 dotnet ef database update
-ECHO --> GATA!
+ECHO  GATA!
 GOTO MENU
 
 
 :MYSQL
-ECHO --> Deschid MySQL CLI...
+ECHO Deschid MySQL CLI...
 docker exec -it fox_shelter_db mysql -u root -pRootPassword123! -D FoxShelterDB
 GOTO MENU
 
-:EOF
 exit
